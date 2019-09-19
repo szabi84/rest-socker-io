d005cd32590441f64f78f3542feda9e6a8b36fbd
Gergye Szabolcs <szabolcs.gergye@attrecto.com>
2019-09-19 15:59:12 +0200

    socket

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {ChatGateway} from "./chat/chat.gateway";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly chatGateway: ChatGateway) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  public async pingClients(): Promise<string> {
+    await this.chatGateway.broadcast("test message");
+    return 'broadcast'
   }
 }
--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,20 +1,37 @@
 import {
-  OnGatewayInit,
-  SubscribeMessage,
-  WebSocketGateway,
+    OnGatewayConnection, OnGatewayDisconnect,
+    OnGatewayInit,
+    SubscribeMessage,
+    WebSocketGateway, WebSocketServer,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({namespace: 'chat'})
-export class ChatGateway implements OnGatewayInit {
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   private logger: Logger = new Logger('AppGateway');
+
+  @WebSocketServer() server;
 
   afterInit(server: any) {
     this.logger.log('initalized');
   }
 
+    async handleConnection(client) {
+        // Notify connected clients of current users
+        this.server.emit("users", 'user connected');
+    }
+
+    async handleDisconnect() {
+        // Notify connected clients of current users
+        this.server.emit("users", 'user disconnected');
+    }
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
   }
+
+    public async broadcast(message: string) {
+        this.server.emit("broadcast", message);
+    }
 }
